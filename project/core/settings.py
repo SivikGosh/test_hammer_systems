7b087eda68d7322266afc0fb91e7d255581cf47d
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2025-07-23 20:57:48 +0300

    0.1.0: Инициализация проекта и необходимой для разработки инфраструктуры.

---
+++ project/core/settings.py
@@ -0,0 +1,91 @@
+from os import getenv
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = getenv('SECRET_KEY', default='$3cR3t-K3y')
+
+DEBUG = getenv('DEBUG', default=True)
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'core.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'core.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv('DB_NAME', default='postgres'),
+        'USER': getenv('DB_USER', default='postgres'),
+        'PASSWORD': getenv('DB_PASSWORD', default='postgres'),
+        'HOST': getenv('DB_HOST', default='localhost'),
+        'PORT': getenv('DB_PORT', default=5432)
+    }
+}
+
+validation = 'django.contrib.auth.password_validation'
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': f'{validation}.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': f'{validation}.MinimumLengthValidator',
+    },
+    {
+        'NAME': f'{validation}.CommonPasswordValidator',
+    },
+    {
+        'NAME': f'{validation}.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Europe/Minsk'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
