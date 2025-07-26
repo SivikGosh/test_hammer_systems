ecf0ea789fbb9f614e76012c68de99bab239b3ff
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2025-07-26 07:47:38 +0300

    0.8.2: Фикс передачи csrf-токена.

--- project/static/js/config.js
+++ project/static/js/config.js
@@ -1,7 +1,10 @@
+const csrftoken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
 export const AppConfig = {
-    csrftoken: '{{ csrf_token }}',
+    csrftoken: csrftoken,
     apiUrl: 'http://localhost:8000',
     phoneRegex: /^\+?[1-9]\d{9,14}$/,
     codeRegex: /^\d{4}$/,
+    activateCodeRegex: /^(\d{6}|\w{6})$/,
     codeTTL: 300,
 };
